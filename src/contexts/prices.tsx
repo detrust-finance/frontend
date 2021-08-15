c269deb6b893e5def83004a2fcec8a28a472eb6c
Web Ninja <x7y8tt@gmail.com>
2021-08-15 14:07:55 +0800

    Move update-prices from API to frontend

--- src/contexts/prices.tsx
+++ src/contexts/prices.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useQuery, UseQueryResult } from 'react-query'
 import { ETH_ADDRESS, IApiDataResponse } from '../constants'
+import fetchPrices from '../libs/misc/update-prices'
 
 const initialState: IApiDataResponse = {
   contractTrustTokens: {
@@ -21,11 +22,12 @@ const PricesProvider: React.FC = ({ children }) => {
   const { data }: UseQueryResult<any, any> = useQuery(
     'update prices',
     async () => {
-      const request = await fetch('/api/update-prices')
-      const result: {
-        status_code: number
-        data: IApiDataResponse
-      } = await request.json()
+      // const request = await fetch('/api/update-prices')
+      // const result: {
+      //   status_code: number
+      //   data: IApiDataResponse
+      // } = await request.json()
+      const result = await fetchPrices()
 
       return result.data
     },
