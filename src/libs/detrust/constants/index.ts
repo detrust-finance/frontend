8c53ee32759e41e07c097cc1dd4c7528f1386397
Web Ninja <x7y8tt@gmail.com>
2021-09-20 15:59:47 +0800

    Add contract address for mainnet

--- src/libs/detrust/constants/index.ts
+++ src/libs/detrust/constants/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '../../wallet'
 import DETRUST_ABI from './abis/detrust.json'
 
 const DETRUST_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: process.env.NEXT_PUBLIC_DETRUST_CONTRACT_ADDRESS_MAINNET ?? '',
+  [ChainId.MAINNET]: process.env.NEXT_PUBLIC_DETRUST_CONTRACT_ADDRESS_MAINNET ?? '0x1060f5077d75D4D71e50E9080AF8b238143f2F5f',
   [ChainId.ROPSTEN]: process.env.NEXT_PUBLIC_DETRUST_CONTRACT_ADDRESS_ROPSTEN ?? '',
   [ChainId.KOVAN]: process.env.NEXT_PUBLIC_DETRUST_CONTRACT_ADDRESS_KOVAN ?? '',
   [ChainId.RINKEBY]: process.env.NEXT_PUBLIC_DETRUST_CONTRACT_ADDRESS_RINKEBY ?? '',
