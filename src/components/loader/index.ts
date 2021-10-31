b9a271b99ddb87e6f88236cb7a27ef06fc4d0269
Web Ninja <x7y8tt@gmail.com>
2021-11-01 02:32:15 +0800

    Change loader style

--- src/components/loader/index.ts
+++ src/components/loader/index.ts
@@ -1,17 +1,31 @@
 import styled, { keyframes } from 'styled-components'
-import { Circle as FeatherLoader } from 'react-feather'
+// import { Circle as FeatherLoader } from 'react-feather'
 
 const loaderAnimation = keyframes`
   100% {
     transform: rotate(360deg);
   }
 `
-export const Loader = styled(FeatherLoader)<{ size?: number }>`
+
+// export const Loader = styled(FeatherLoader)<{ size?: number }>`
+//   animation: ${loaderAnimation} 1.5s ease-in-out infinite;
+//   /*stroke: #29b500;*/
+//   stroke: #F0864B;
+//   height: ${({ size }) => size};
+//   stroke-linecap: round;
+//   stroke-width: 1.4;
+//   stroke-linejoin: round;
+//   stroke-dasharray: 45;
+// `
+
+export const Loader = styled.div<{ size: number }>`
   animation: ${loaderAnimation} 1.5s ease-in-out infinite;
-  stroke: #29b500;
-  height: ${({ size }) => size};
-  stroke-linecap: round;
-  stroke-width: 1.4;
-  stroke-linejoin: round;
-  stroke-dasharray: 45;
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  border-style: solid;
+  border-color: rgba(0, 0, 0, 0.1);
+  border-bottom-color: #F0864B;
+  border-width: 5px;
+  border-radius: 50%;
+  box-sizing: border-box;
 `
