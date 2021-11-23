62227852f8c6c076ccde0425aaf48c3b36dfacd0
Web Ninja <x7y8tt@gmail.com>
2021-11-24 04:56:58 +0800

    Fix for create-new-trust

--- src/pages/dashboard/settlor/create-new-trust/step-one.tsx
+++ src/pages/dashboard/settlor/create-new-trust/step-one.tsx
@@ -101,7 +101,7 @@ const StepOne: React.FC<FullData> = ({ setForm, formData, navigation }) => {
             }
       }
     ],
-    [isTablet, navigation?.next, t]
+    [isTablet, t]
   )
 
   const onSubmit = React.useCallback(
